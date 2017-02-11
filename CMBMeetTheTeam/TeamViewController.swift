2b6f21c6bcb59fffc0d49216018b0dcfbc95474f
Forrest Zhao <fozhao92@gmail.com>
2017-02-11 14:37:20 -0800

    add pods, make teamVC initial VC of navVC in appdelegate

---
+++ CMBMeetTheTeam/TeamViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CMBMeetTheTeam
+//
+//  Created by Forrest Zhao on 2/11/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import SnapKit
+
+
+
+class TeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CMBMeetTheTeam/Pods-CMBMeetTheTeam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CMBMeetTheTeam : NSObject
+@end
+@implementation PodsDummy_Pods_CMBMeetTheTeam
+@end
